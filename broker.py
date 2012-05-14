df565f8b7121b7f9565be48569319aeea02581eb
Yannick Hold-Geoffroy <xgodsoul@gmail.com>
2012-05-14 17:39:18 -0400

    Added forgottent licence header

--- broker.py
+++ broker.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+#
+#    This file is part of Scalable COncurrent Operations in Python (SCOOP).
+#
+#    SCOOP is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as
+#    published by the Free Software Foundation, either version 3 of
+#    the License, or (at your option) any later version.
+#
+#    SCOOP is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with SCOOP. If not, see <http://www.gnu.org/licenses/>.
+#
 from __future__ import print_function
 import time
 import cPickle as pickle
