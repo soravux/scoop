3de9a9a470dee87fedc230e1fa532ccac4859184
Yannick Hold-Geoffroy <yannick.hold-geoffroy.1@ulaval.ca>
2012-05-14 01:01:43 -0400

    Overhaul of scooprun.py in order to be more flexible over remote and local hosts. Works only in master-slave in mirror mode as of now. Updated the documentation to reflect the script modifications.

--- doc/readme.rst
+++ doc/readme.rst
@@ -18,7 +18,7 @@ Meta socket Extraneous Publisher-Subscriber socket created between the brokers t
 =========== ==================================================================================================
 
 
-How to use SCOOP in your code 
+How to use SCOOP in your code
 -----------------------------
 
 The philosophy of SCOOP is build around the *futures* module proposed by :pep:`3148`. It defines ``map()`` and ``join()`` functions allowing asynchroneous computation which SCOOP will propagate to it's local and foreign workers.
@@ -30,7 +30,38 @@ You should also be aware that your main function, meaning your parent function t
 How to launch a task
 --------------------
 
-The script ``scooprun.py`` alows you to automatically spawn the needed brokers and workers on a given list of computer, including remote ones via ssh. It will automatically generate the environment variables needed for the execution of the parallel task.
+Requirements
+~~~~~~~~~~~~
+
+You may want to run your distributed and parallel tasks over a tightly integrated grid such as a supercomputer or you may want to test a bunch of computers laying there in the laboratory of put to good use some heterogeneous systems at home. SCOOP allows you to scale your parallel tasks to all these situations without modification to your source code.
+
+* Python >= 2.6
+* Greenlets >= 0.3.4
+* PyZMQ as well as libzmq >= 2.2.0
+
+SCOOP works with Linux and Windows. On the latter, be sure to have Cygwin installed and your PATH environment variable setted correctly in order to be able to launch tasks remotely, since this feature uses ``ssh``.
+
+If you can't modify the system-wide python installation, you should install the requirements using::
+
+    python setup.py install --prefix=/home/your_username/your_desired_python_lib_path/
+
+This way, you will be able to invoke your program after setting the environment variable (``export PYTHONPATH=$PYTHONPATH:/home/your_username/your_desired_python_lib_path/`` for the given example).
+
+scooprun.py script
+~~~~~~~~~~~~~~~~~~
+
+The script ``scooprun.py`` alows you to automatically spawn the needed brokers and workers on a given list of computer, including remote ones via ssh. It will automatically generate the environment variables needed for the execution of the parallel task on the remote workers.
+
+You can pass the argument ``--help`` to scooprun.py to get the list of available arguments.
+
+An usage example may be as follow::
+
+    python ./scooprun.py --hosts 192.168.1.101 192.168.1.102 192.168.1.103 -vv -N 16 your_program.py
+
+This will run 4 workers on each 3 remotes hosts as well as the local machine that will execute ``you_program.py``.
+
+Manual launch
+~~~~~~~~~~~~~
 
 To start a parallel task manually, you must launch a minimum of one broker. No workers are mandatory to execute a task with SCOOP, though it would defeat its purpose. Start any of the desired worker with its environment variables defining how to connect to it's broker. The following environment variables are used by the workers:
 
@@ -39,14 +70,15 @@ To start a parallel task manually, you must launch a minimum of one broker. No w
 Environment variables for the workers
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-====================  ============================================================================================  ========================
-Environment Variable  Description                                                                                   Default value
-====================  ============================================================================================  ========================
-IS_ORIGIN             Set it to 1 if the worker is the origin of the task.                                          1
-WORKER_NAME           The name of the current worker.                                                               origin
-BROKER_NAME           The name of the broker the current worker will connect to.                                    broker
-BROKER_ADDRESS        The address of the broker with protocol and port that the current worker will connect to.     ``tcp://127.0.0.1:5555``
-====================  ============================================================================================  ========================
+====================  =====================================================================================================  ========================
+Environment Variable  Description                                                                                            Default value
+====================  =====================================================================================================  ========================
+IS_ORIGIN             Set it to 1 if the worker is the origin of the task.                                                   1
+WORKER_NAME           The name of the current worker.                                                                        origin
+BROKER_NAME           The name of the broker the current worker will connect to.                                             broker
+BROKER_ADDRESS        The address of the broker task socket with protocol and port that the current worker will connect to.  ``tcp://127.0.0.1:5555``
+META_ADDRESS          The address of the broker meta socket with protocol and port that the current worker will connect to.  ``tcp://127.0.0.1:5556``
+====================  =====================================================================================================  ========================
 
 .. _Environment-variables-for-the-brokers:
 
--- scooprun.py
+++ scooprun.py
@@ -6,6 +6,12 @@ import time
 import os
 import socket
 
+def log(text, level=0):
+    """Easily logs on screen the different events happening based on the
+    verbosity level"""
+    if args.verbose > level-1:
+        print("[{0}]\tscooprun -> {1}".format(time.time(), text))
+
 cwd = os.getcwd()
     
 parser = argparse.ArgumentParser(description='Starts the executable on the nodes.',
@@ -25,66 +31,96 @@ parser.add_argument('--verbose', '-v',
 parser.add_argument('-N',
                     help="Number of process",
                     type=int)
+parser.add_argument('-b',
+                    help="Don't start additional workers on the local machine, only the broker and the origin",
+                    action='store_true')
 parser.add_argument('executable',
                     nargs='+',
                     help='The executable to start with scoop and its arguments')
+parser.add_argument('--broker-hostname',
+                    nargs=1,
+                    help='The broker hostname (from outside)')
+parser.add_argument('--python-executable',
+                    nargs=1,
+                    help='The python executable with which to execute the script (including path if necessary)',
+                    default=["python"])
 args = parser.parse_args()
 
 hosts = [] if args.hosts == None else args.hosts
-hosts += ['127.0.0.1'] # Ensure local machine is in the list
+hosts += ['127.0.0.1'] if not args.b else []
+hosts = set(hosts)
+workers_left = args.N
 created_subprocesses = []
 
-workers_left = args.N
-maximum_workers = {'127.0.0.1': 32,}
+# Division of workers pseudo-equally upon the hosts
+maximum_workers = {}
+for index, host in enumerate(hosts):
+    maximum_workers[host] = (args.N / (len(hosts)) + int((args.N % len(hosts)) > index))
 
-# Remove link-local and loopback (see RFC 5735 and RFC 3330)
-broker_ip = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.") and not ip.startswith("169.254.")][0]
-if args.verbose > 0: print("[{0}]\tscooprun -> Using IPv4 {1} address for broker.".format(time.time(), broker_ip))
+if args.broker_hostname == None:
+    # Remove link-local and loopback from interfaces (see RFC 5735 and RFC 3330)
+    broker_ip = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.") and not ip.startswith("169.254.")][0]
+    log('Using IPv4 {0} address for broker.'.format(broker_ip), 1)
+else:
+    broker_ip = args.broker_hostname[0]
 
 try:
+    # Launching the local broker
+    log('Initialising local broker.', 1)
+    created_subprocesses.append(subprocess.Popen([args.python_executable[0], 'broker.py']))
+    # Let's wait until the local broker is up and running...
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    begin = time.time()
+    while(time.time() - begin < 10.0):
+        time.sleep(0.1)
+        try:
+            s.connect(('127.0.0.1', 5555))
+            s.shutdown(2)
+            break
+        except:
+            pass
+    else:
+        raise Exception('Could not start server!')
+            
     for index, host in enumerate(hosts):
         if host in ["127.0.0.1", "localhost"]:
-            if args.verbose > 0: print("[{0}]\tscooprun -> Initialising local \
-element of the federation.".format(time.time()))
-            # Launching the broker
-            if args.verbose > 1: print("[{0}]\tscooprun -> Initialising local \
-broker.".format(time.time()))
-            created_subprocesses.append(subprocess.Popen(['python', 'broker.py']))
-            time.sleep(1) # Get something better than this?
-            
             # Launching the workers
-            for n in range(min(maximum_workers['127.0.0.1'], workers_left) + 1):
-                if args.verbose > 1: print("[{0}]\tscooprun -> Initialising local \
-worker.".format(time.time()))
+            log('Initialising local workers attached to the local broker...', 1)
+            for n in range(min(maximum_workers['127.0.0.1'], workers_left)):
+                log('Initialising local worker {0}.'.format(n), 2)
                 os.environ.update({'WORKER_NAME': 'worker{0}'.format(n),
                                    'IS_ORIGIN': '0'})
-                created_subprocesses.append(subprocess.Popen(['python'] \
+                created_subprocesses.append(subprocess.Popen([args.python_executable[0]] \
                     + args.executable))
                 workers_left -= 1
         else:
-            if args.verbose > 0: print("[{0}]\tscooprun -> Initialising remote \
-element of the federation <{1}>.".format(time.time(), host))
             # If the host is remote, connect with ssh
-            shell = subprocess.Popen(['ssh', '-x', '-n', host,
-                                      'IS_ORIGIN=0 WORKER_NAME={3} BROKER_NAME=broker BROKER_ADDRESS=tcp://{2}:5555 META_ADDRESS=tcp://{2}:5556 python {0} {1}'.format(
-                                        os.path.join(args.path, args.executable[0]),
-                                        " ".join(args.executable[1:]),
-                                        broker_ip,
-                                        workers_left)])
-            workers_left -= 1
+            log('Initialising remote workers of host {0} attached to the local broker...'.format(host), 1)
+            for a in range(min(maximum_workers.get(host, 8), workers_left)):
+                log('Initialising remote worker {0}'.format(workers_left), 2)
+                command = 'PYTHONPATH={5} IS_ORIGIN=0 WORKER_NAME=worker{3}\
+                BROKER_NAME=broker BROKER_ADDRESS=tcp://{2}:5555 META_ADDRESS=tcp://{2}:5556 {4} {0} {1}'.format(
+                    os.path.join(args.path, args.executable[0]),
+                    " ".join(args.executable[1:]),
+                    broker_ip,
+                    workers_left,
+                    args.python_executable[0],
+                    os.environ.get("PYTHONPATH", ":"))
+                # TODO: start every worker in one SSH channel.
+                shell = subprocess.Popen(['ssh', '-x', '-n',
+                                          '-oStrictHostKeyChecking=no', host, '-p24680', 
+                                          command])
+                workers_left -= 1
+        if workers_left <= 0: break
     # Everything has been started everywhere, we can then launch our origin.
-    # TODO: the origin could be anywhere else
-    if args.verbose > 1: print("[{0}]\tscooprun -> Initialising local \
-origin.".format(time.time()))
+    log('Initialising local origin.', 1)
     os.environ.update({'WORKER_NAME': 'root',
                        'IS_ORIGIN': '1'})
-    created_subprocesses.append(subprocess.call(['python'] + args.executable))
+    created_subprocesses.append(subprocess.call([args.python_executable[0]] + args.executable))
 finally:
     # Ensure everything is cleaned up on exit
-    if args.verbose > 0: print("[{0}]\tscooprun -> Destroying local \
-element of the federation.".format(time.time()))
+    log('Destroying local elements of the federation...', 1)
     for process in created_subprocesses:
         try: process.kill()
         except: pass
-    if args.verbose > 0: print("[{0}]\tscooprun -> Destroyed subprocesses."\
-        .format(time.time()))
+    log('Finished destroying spawned subprocesses.', 2)
