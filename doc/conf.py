bf49f188e115a057ef52bdccf537bfa11d5c4804
Yannick Hold-Geoffroy <yannickhold@gmail.com>
2012-05-16 19:15:05 -0400

    Fixed documentation configuration error

--- doc/conf.py
+++ doc/conf.py
@@ -12,12 +12,13 @@
 # serve to show the default.
 
 import sys, os
-import scoop
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.join(os.path.abspath('.'), ".."))
+import scoop
+
 
 # -- General configuration -----------------------------------------------------
 
