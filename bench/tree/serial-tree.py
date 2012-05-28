b23b9b0f1dea42027854ebfa9679718fdb21387b
Olivier Gagnon <olivier.gagnon.7@ulaval.ca>
2012-05-28 12:12:55 -0400

    Corrected the examples for python3 and python2

---
+++ bench/tree/serial-tree.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#import treeTester
+from treeTester import *
+
+
+filename = "tree.txt"
+
+def main():
+    return executeTree([])
+
+if __name__=="__main__":
+    importTree(filename)
+    registerMap(map)
+    print(main())
