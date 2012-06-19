30a8e96de8a83078ba94dca9b24d4dcecf12d53f
Yannick Hold-Geoffroy <yannickhold@gmail.com>
2012-06-19 17:03:54 -0400

    Enhanced documentation

---
+++ examples/tree/scoop_tree.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from treeTester import *
+import sys
+from scoop import futures
+
+def main():
+    return executeTree()
+
+if __name__=="__main__":
+    importTree(sys.argv[1] if len(sys.argv) > 1 else "tree.txt")
+    registerMap(futures.map)
+    print(main())
