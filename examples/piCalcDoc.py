3f86dd5d4ca8043682b89f5eb37339129530b684
Olivier Gagnon <olivier.gagnon737@gmail.com>
2012-08-01 09:32:54 -0400

    Prototype of a better method to get the basic host.

---
+++ examples/piCalcDoc.py
@@ -0,0 +1,34 @@
+#
+#    This file is part of Scalable COncurrent Operations in Python (SCOOP).
+#
+#    SCOOP is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as
+#    published by the Free Software Foundation, either version 3 of
+#    the License, or (at your option) any later version.
+#
+#    SCOOP is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with SCOOP. If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+Calculation of Pi using a Monte Carlo method. This version is used in the 
+documentation and should be kept as clean as possible for study purposes.
+"""
+
+from math import hypot
+from random import random
+from scoop import futures
+
+def test(tries):
+    return sum(hypot(random(), random()) < 1 for i in range(tries))
+
+def calcPi(n, t):
+    expr = futures.map(test, [t] * n)
+    return 4. * sum(expr) / float(n * t)
+
+if __name__ == "__main__":
+    print("pi = {}".format(calcPi(3000, 5000)))
