b2cacef78348c6483053e65dc3335ce50ac1acb1
Yannick Hold <yannickhold@gmail.com>
2012-08-11 00:55:37 -0400

    Modified signal for another windows-compliant.

--- scoop/__main__.py
+++ scoop/__main__.py
@@ -27,7 +27,7 @@ from scoop import utils
 from threading import Thread
 import signal
 
-signal.signal(signal.SIGQUIT, utils.KeyboardInterruptHandler)
+signal.signal(signal.SIGTERM, utils.KeyboardInterruptHandler)
 
 class launchScoop(object):
     def __init__(self, hosts, n, verbose, python_executable, brokerHostname,
