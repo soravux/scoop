7a63346e68e6493cd2ef188270252670e16f0c5a
Yannick Hold <yannickhold@gmail.com>
2012-08-13 10:43:33 -0400

    Made __main__.py pep8 compliant.

--- scoop/__main__.py
+++ scoop/__main__.py
@@ -29,11 +29,13 @@ import signal
 
 signal.signal(signal.SIGTERM, utils.KeyboardInterruptHandler)
 
+
 class launchScoop(object):
     def __init__(self, hosts, n, verbose, python_executable, brokerHostname,
-            executable, arguments, e, log, path, debug, nice, env):
+                 executable, arguments, e, log, path, debug, nice, env):
         # Assure setup sanity
-        assert type(hosts) == list and hosts != [], "You should at least specify one host."
+        assert type(hosts) == list and hosts != [], ("You should at least "
+                                                     "specify one host.")
         hosts.reverse()
         self.workersLeft = n
         self.createdSubprocesses = []
@@ -57,8 +59,9 @@ class launchScoop(object):
                           2: logging.DEBUG}
         logging.basicConfig(filename=log,
                             level=verbose_levels[self.verbose],
-                            format='[%(asctime)-15s] %(levelname)-7s %(message)s')
-                            
+                            format='[%(asctime)-15s] %(levelname)-7s '
+                                   '%(message)s')
+
         if env in ["PBS", "SGE"]:
             logging.info("Detected {0} environment.".format(env))
         logging.info("Deploying {0} workers over {1} "
@@ -66,32 +69,29 @@ class launchScoop(object):
                                        len(hosts)))
 
         self.divideHosts(hosts)
-     
+
         # Handling Broker Hostname
         self.brokerHostname = '127.0.0.1' if self.e else brokerHostname[0]
-        logging.debug('Using hostname/ip: "{0}" as external broker reference.'\
-            .format(self.brokerHostname))
-        logging.info('The python executable to execute the program with is: {0}.'\
-            .format(self.python_executable))
-
-   
+        logging.debug('Using hostname/ip: "{0}" as external broker '
+                      'reference.'.format(self.brokerHostname))
+        logging.info('The python executable to execute the program with is: '
+                     '{0}.'.format(self.python_executable))
 
     def launchLocal(self):
         c = [self.python_executable,
-                        "-m", "scoop.bootstrap.__main__",
-                        "--workerName", "worker{0}".format(self.workersLeft),
-                        "--brokerName", "broker",
-                        "--brokerAddress",
-                        "tcp://{0}:{1}".format(self.brokerHostname,
-                                               self.brokerPort),
-                        "--metaAddress",
-                        'tcp://{0}:{1}'.format(self.brokerHostname,
-                                               self.infoPort),
-                        "--size", str(self.n),
-                        ]
+             "-m", "scoop.bootstrap.__main__",
+             "--workerName", "worker{0}".format(self.workersLeft),
+             "--brokerName", "broker",
+             "--brokerAddress",
+             "tcp://{0}:{1}".format(self.brokerHostname,
+                                    self.brokerPort),
+             "--metaAddress",
+             'tcp://{0}:{1}'.format(self.brokerHostname,
+                                    self.infoPort),
+             "--size", str(self.n)]
         if self.workersLeft == 1:
             c.append("--origin")
-        if self.debug == True:
+        if self.debug is True:
             logging.debug('Set debug on')
             c.append("--debug")
         c.append(self.executable)
@@ -104,27 +104,30 @@ class launchScoop(object):
                 "--brokerName broker --brokerAddress tcp://{brokerHostname}:"
                 "{brokerPort} --metaAddress tcp://{brokerHostname}:"
                 "{infoPort} --size {n} {origin} {debug} {executable} "
-                "{arguments}").format(remotePath = self.path, 
-                    nice = 'nice - n {0}'.format(self.nice) if self.nice != None else '',
-                    origin = '--origin' if self.workersLeft == 1 else '',
-                    debug = '--debug' if self.debug == 1 else '',
-                    pythonExecutable = self.python_executable,
-                    workersLeft = self.workersLeft,
-                    brokerHostname = self.brokerHostname,
-                    brokerPort = self.brokerPort,
-                    infoPort = self.infoPort,
-                    n = self.n,
-                    executable = self.executable,
-                    arguments = " ".join(self.args))
+                "{arguments}").format(
+                    remotePath=self.path,
+                    nice='nice - n {0}'.format(self.nice)
+                    if self.nice is not None else '',
+                    origin='--origin' if self.workersLeft == 1 else '',
+                    debug='--debug' if self.debug == 1 else '',
+                    pythonExecutable=self.python_executable,
+                    workersLeft=self.workersLeft,
+                    brokerHostname=self.brokerHostname,
+                    brokerPort=self.brokerPort,
+                    infoPort=self.infoPort,
+                    n=self.n,
+                    executable=self.executable,
+                    arguments=" ".join(self.args))
 
     def divideHosts(self, hosts):
         """ Separe the workers accross hosts. """
         maximumWorkers = sum(host[1] for host in hosts)
         if self.n > maximumWorkers:
-            logging.info("The -n flag is set at {0} workers, which is higher than\n"
-                         "the maximum number of workers ({1}) specified by the hostfile.\n"
-                         "This behaviour may degrade the performances of scoop for cpu-bound"
-                         " operations.".format(self.n, sum(host[1] for host in hosts)))
+            logging.info("The -n flag is set at {0} workers, which is higher "
+                         "than the maximum number of workers ({1}) specified "
+                         "by the hostfile.\nThis behaviour may degrade the "
+                         "performances of scoop for cpu-bound operations."
+                         "".format(self.n, sum(host[1] for host in hosts)))
         index = 0
         while self.n > maximumWorkers:
             hosts[index] = (hosts[index][0], hosts[index][1] + 1)
@@ -141,11 +144,11 @@ class launchScoop(object):
                     worker,
                     number - 1 if worker == hosts[0][0] else str(number),
                     "+ origin" if worker == hosts[0][0] else ""))
-#
+
     def startBroker(self):
         """Starts a broker on random unoccupied port(s)"""
         from scoop.broker import Broker
-        self.localBroker = Broker(debug=True if self.debug == True else False)
+        self.localBroker = Broker(debug=True if self.debug is True else False)
         self.brokerPort, self.infoPort = self.localBroker.getPorts()
         self.localBrokerProcess = Thread(target=self.localBroker.run)
         self.localBrokerProcess.daemon = True
@@ -157,13 +160,14 @@ class launchScoop(object):
         self.startBroker()
         logging.debug("Local broker launched on ports {0}, {1}"
                       ".".format(self.brokerPort, self.infoPort))
-        
+
         # Launch the workers
         for host in self.hosts:
             command = []
             for n in range(min(host[1], self.workersLeft)):
                 logging.debug('Initialising {0}{1} worker {2} [{3}].'.format(
-                    "local" if host[0] in ["127.0.0.1", "localhost"] else "remote",
+                    "local" if host[0] in ["127.0.0.1", "localhost"]
+                    else "remote",
                     " origin" if self.workersLeft == 1 else "",
                     self.workersLeft,
                     host[0]))
@@ -175,8 +179,8 @@ class launchScoop(object):
                     # If the host is remote, connect with ssh
                     command.append(self.launchForeign())
                 self.workersLeft -= 1
-            # Launch every remote hosts in the same time 
-            if len(command) != 0 :
+            # Launch every remote hosts in the same time
+            if len(command) != 0:
                 ssh_command = ['ssh', '-x', '-n', '-oStrictHostKeyChecking=no']
                 if self.e:
                     ssh_command += [
@@ -188,22 +192,23 @@ class launchScoop(object):
                 self.createdSubprocesses.append(shell)
                 command = []
             if self.workersLeft <= 0:
-                # We've launched every worker we needed, so let's exit the loop!
+                # We've launched every worker we needed, so let's exit the loop
                 break
-                
+
         # Ensure everything is started normaly
         for this_subprocess in self.createdSubprocesses:
             if this_subprocess.poll() is not None:
-                raise Exception('Subprocess {0} terminated abnormaly.'\
-                    .format(this_subprocess))
-        
+                raise Exception('Subprocess {0} terminated abnormaly.'
+                                .format(this_subprocess))
+
         # wait for the origin
         self.createdSubprocesses[-1].wait()
 
     def close(self):
         # Ensure everything is cleaned up on exit
         logging.debug('Destroying local elements...')
-        self.createdSubprocesses.reverse() # Kill the broker last
+        # Kill the broker last
+        self.createdSubprocesses.reverse()
         if self.debug == 1:
             # give time to flush data
             time.sleep(1)
@@ -214,32 +219,31 @@ class launchScoop(object):
                 pass
         logging.info('Finished destroying spawned subprocesses.')
 
-
 parser = argparse.ArgumentParser(description="Starts a parallel program using "
                                              "SCOOP.",
                                  prog="{0} -m scoop".format(sys.executable))
 group = parser.add_mutually_exclusive_group()
 group.add_argument('--hosts', '--host',
-                    help="The list of hosts. The first host will execute the "
-                         "origin. (default is 127.0.0.1)",
-                    nargs='*')
+                   help="The list of hosts. The first host will execute the "
+                        "origin. (default is 127.0.0.1)",
+                   nargs='*')
 group.add_argument('--hostfile', help="The hostfile name")
 parser.add_argument('--path', '-p',
-                    help="The path to the executable on remote hosts  (default "
-                         "is local directory)",
+                    help="The path to the executable on remote hosts "
+                         "(default is local directory)",
                     default=os.getcwd())
 parser.add_argument('--nice',
                     type=int,
                     help="*nix niceness level (-20 to 19) to run the "
                          "executable")
 parser.add_argument('--verbose', '-v',
-                    action = 'count',
-                    help = "Verbosity level of this launch script (-vv for "
+                    action='count',
+                    help="Verbosity level of this launch script (-vv for "
                            "more)",
-                    default = 0)
+                    default=0)
 parser.add_argument('--log',
-                    help = "The file to log the output. (default is stdout)",
-                    default = None)
+                    help="The file to log the output. (default is stdout)",
+                    default=None)
 parser.add_argument('-n',
                     help="Total number of workers to launch on the hosts. "
                          "Workers are spawned sequentially over the hosts. "
@@ -268,19 +272,18 @@ parser.add_argument('--pythonpath',
                     help="The PYTHONPATH environment variable (default is "
                          "current PYTHONPATH)",
                     default=[os.environ.get('PYTHONPATH', '')])
-parser.add_argument('--debug', help="Turn on the debuging", action='store_true')
+parser.add_argument('--debug',
+                    help="Turn on the debuging",
+                    action='store_true')
 parser.add_argument('executable',
                     nargs=1,
                     help='The executable to start with SCOOP')
 parser.add_argument('args',
                     nargs=argparse.REMAINDER,
                     help='The arguments to pass to the executable',
-                    default=[])                   
+                    default=[])
 args = parser.parse_args()
-   
 
-
-        
 if __name__ == "__main__":
     hosts = utils.getHosts(args.hostfile, args.hosts)
     if args.n:
@@ -288,11 +291,12 @@ if __name__ == "__main__":
     else:
         n = utils.getWorkerQte(hosts)
     assert n > 0, ("Scoop couldn't determine the number of worker to start.\n"
-                   "Use the '-n' flag to set it manually.") 
+                   "Use the '-n' flag to set it manually.")
     scoopLaunching = launchScoop(hosts, n, args.verbose,
-            args.python_executable, args.broker_hostname, args.executable,
-            args.args, args.e, args.log, args.path, args.debug, args.nice,
-            utils.getEnv())
+                                 args.python_executable, args.broker_hostname,
+                                 args.executable, args.args, args.e, args.log,
+                                 args.path, args.debug, args.nice,
+                                 utils.getEnv())
     try:
         scoopLaunching.run()
     finally:
