08f6849046c95115b93667fd7a8148a22f2e7fc9
stdweird <stdweird@localhost>
2013-01-07 14:14:29 -0500

    refactor bootstrap module

--- scoop/bootstrap/__main__.py
+++ scoop/bootstrap/__main__.py
@@ -26,127 +26,141 @@ import argparse
 import scoop
 
 
-def makeParser():
-    parser = argparse.ArgumentParser(description='Starts the executable.',
-                                     prog=("{0} -m scoop.bootstrap"
-                                           ).format(sys.executable))
+class Bootstrap(object):
+    def __init__(self):
+        self.parser = None
+        self.args = None
 
-    parser.add_argument('--origin',
-                        help="To specify that the worker is the origin",
-                        action='store_true')
-    parser.add_argument('--workerName', help="The name of the worker",
-                        default="0")
-    parser.add_argument('--brokerName', help="The name of the broker",
-                        default="broker")
-    parser.add_argument('--brokerAddress',
-                        help="The tcp address of the broker written "
-                             "tcp://address:port",
-                        default="")
-    parser.add_argument('--metaAddress',
-                        help="The tcp address of the info written "
-                             "tcp://address:port",
-                        default="")
-    parser.add_argument('--size',
-                        help="The size of the worker pool",
-                        type=int,
-                        default=1)
-    parser.add_argument('--debug',
-                        help="Activate the debug",
-                        action='store_true')
-    parser.add_argument('--profile',
-                        help="Activate the profiler",
-                        action='store_true')
-    parser.add_argument('executable',
-                        nargs=1,
-                        help='The executable to start with scoop')
-    parser.add_argument('args',
-                        nargs=argparse.REMAINDER,
-                        help='The arguments to pass to the executable',
-                        default=[])
-    parser.add_argument('--echoGroup',
-                        help="Echo the process Group ID before launch",
-                        action='store_true')
-    return parser
+    def main(self):
+        if self.args is None:
+            self.parse()
 
+        self.setScoop()
 
-def main():
-    # Generate a argparse parser and parse the command-line arguments
-    parser = makeParser()
-    args = parser.parse_args()
+        self.run()
 
-    # Setup the SCOOP constants
-    scoop.IS_ORIGIN = args.origin
-    scoop.WORKER_NAME = args.workerName.encode()
-    scoop.BROKER_NAME = args.brokerName.encode()
-    scoop.BROKER_ADDRESS = args.brokerAddress.encode()
-    scoop.META_ADDRESS = args.metaAddress.encode()
-    scoop.SIZE = args.size
-    scoop.DEBUG = args.debug
-    scoop.worker = (scoop.WORKER_NAME, scoop.BROKER_NAME)
+    def makeParser(self):
+        self.parser = argparse.ArgumentParser(description='Starts the executable.',
+                                              prog=("{0} -m scoop.bootstrap"
+                                                    ).format(sys.executable))
 
-    if scoop.DEBUG:
-        from scoop import _debug
+        self.parser.add_argument('--origin',
+                                 help="To specify that the worker is the origin",
+                                 action='store_true')
+        self.parser.add_argument('--workerName', help="The name of the worker",
+                                 default="0")
+        self.parser.add_argument('--brokerName', help="The name of the broker",
+                                 default="broker")
+        self.parser.add_argument('--brokerAddress',
+                                 help="The tcp address of the broker written "
+                                    "tcp://address:port",
+                                 default="")
+        self.parser.add_argument('--metaAddress',
+                                 help="The tcp address of the info written "
+                                    "tcp://address:port",
+                                 default="")
+        self.parser.add_argument('--size',
+                                 help="The size of the worker pool",
+                                 type=int,
+                                default=1)
+        self.parser.add_argument('--debug',
+                                 help="Activate the debug",
+                                 action='store_true')
+        self.parser.add_argument('--profile',
+                                 help="Activate the profiler",
+                                 action='store_true')
+        self.parser.add_argument('executable',
+                                 nargs=1,
+                                 help='The executable to start with scoop')
+        self.parser.add_argument('args',
+                                 nargs=argparse.REMAINDER,
+                                 help='The arguments to pass to the executable',
+                                 default=[])
+        self.parser.add_argument('--echoGroup',
+                                 help="Echo the process Group ID before launch",
+                                 action='store_true')
 
-    profile = True if args.profile else False
+    def parse(self):
+        # Generate a argparse parser and parse the command-line arguments
+        self.parser = self.makeParser()
+        if self.parser is None:
+            self.makeParser()
+        self.args = self.parser.parse_args()
 
-    # get the module path in the Python path
-    sys.path.append(os.path.join(os.getcwd(),
-                    os.path.dirname(args.executable[0])))
+    def setScoop(self):
+        # Setup the SCOOP constants
+        scoop.IS_ORIGIN = self.args.origin
+        scoop.WORKER_NAME = self.args.workerName.encode()
+        scoop.BROKER_NAME = self.args.brokerName.encode()
+        scoop.BROKER_ADDRESS = self.args.brokerAddress.encode()
+        scoop.META_ADDRESS = self.args.metaAddress.encode()
+        scoop.SIZE = self.args.size
+        scoop.DEBUG = self.args.debug
+        scoop.worker = (scoop.WORKER_NAME, scoop.BROKER_NAME)
 
-    # temp values to keep the args
-    executable = args.executable[0]
+        if scoop.DEBUG:
+            from scoop import _debug
 
-    # Add the user arguments to argv
-    sys.argv = sys.argv[:1]
-    sys.argv += args.args
+    def run(self):
+        # get the module path in the Python path
+        sys.path.append(os.path.join(os.getcwd(),
+                        os.path.dirname(self.args.executable[0])))
 
-    # Show the current process Group ID if asked
-    if args.echoGroup:
-        sys.stdout.write(str(os.getpgrp()) + "\n")
-        sys.stdout.flush()
+        # temp values to keep the args
+        executable = self.args.executable[0]
 
-    # import the user module into the global dictionary
-    # equivalent to from {user_module} import *
-    try:
-        user_module = __import__(os.path.basename(executable)[:-3])
-    except ImportError as e:
-        # Could not find 
-        sys.stderr.write('{0}\nIn path: {1}\n'.format(
-            str(e),
-            sys.path[-1],
-            )
-        )
-        sys.stderr.flush()
-        sys.exit(-1)
-    try:
-        attrlist = user_module.__all__
-    except AttributeError:
-        attrlist = dir(user_module)
-    for attr in attrlist:
-        globals()[attr] = getattr(user_module, attr)
+        # Add the user arguments to argv
+        sys.argv = sys.argv[:1]
+        sys.argv += self.args.args
 
-    # Start the user program
-    from scoop import futures
-    if not profile:
-        futures._startup(
-            functools.partial(
-                runpy.run_path,
-                executable,
-                init_globals=globals(),
-                run_name="__main__"
-            )
-        )
-    else:
-        import cProfile
-        cProfile.run("""futures._startup(
-                functools.partial(
-                    runpy.run_path,
-                   "{0}",
-                   init_globals=globals(),
-                   run_name="__main__",
+        # Show the current process Group ID if asked
+        if self.args.echoGroup:
+            sys.stdout.write(str(os.getpgrp()) + "\n")
+            sys.stdout.flush()
+
+        # import the user module into the global dictionary
+        # equivalent to from {user_module} import *
+        try:
+            user_module = __import__(os.path.basename(executable)[:-3])
+        except ImportError as e:
+            # Could not find
+            sys.stderr.write('{0}\nIn path: {1}\n'.format(
+                str(e),
+                sys.path[-1],
                 )
-            )""".format(executable),
-            scoop.WORKER_NAME)
+            )
+            sys.stderr.flush()
+            sys.exit(-1)
+        try:
+            attrlist = user_module.__all__
+        except AttributeError:
+            attrlist = dir(user_module)
+        for attr in attrlist:
+            globals()[attr] = getattr(user_module, attr)
+        # Start the user program
+        from scoop import futures
+        def futures_startup():
+            return futures._startup(
+                                    functools.partial(
+                                                      runpy.run_path,
+                                                      executable,
+                                                      init_globals=globals(),
+                                                      run_name="__main__"
+                                                      )
+                                    )
+
+        if self.args.profile:
+            import cProfile
+            # runctx instead of run is required for local function
+            cProfile.runctx("futures_startup()",
+                            globals(),
+                            locals(),
+                            "{0}.prof".format(scoop.WORKER_NAME)
+                            )
+        else:
+            futures_startup()
+
 
 if __name__ == "__main__":
-    main()
+    b = Bootstrap()
+    b.main()
