5d931e6a56ee4c3c936d84f00522819ca39b71a4
Olivier Gagnon <olivier.gagnon737@gmail.com>
2013-05-21 11:27:08 -0400

    Added new unittest for small utility classes

---
+++ test/tests_stat.py
@@ -0,0 +1,37 @@
+from scoop._control import _stat
+
+import unittest
+
+class TestStat(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestStat, self).__init__(*args, **kwargs)
+
+    def test_appendleft(self):
+        stats = _stat()
+        data = list(range(5))
+        for i in data[::-1]:
+            stats.appendleft(i)
+        for i, j in enumerate(stats):
+            self.assertEqual(data[i], j)
+     
+    def test_maxlen(self):
+        stats = _stat()
+        data = list(range(15))
+        for i in data[::-1]:
+            stats.appendleft(i)
+        for i, j in enumerate(stats):
+            self.assertEqual(data[i], j)
+        self.assertEqual(len(stats), 10)
+
+    def test_mean(self):
+        stats = _stat()
+        data = list(range(15))
+        for i in data:
+            stats.appendleft(i)
+        self.assertEqual(stats.mean(), 9.5)
+        stats.appendleft(1000)
+        self.assertEqual(stats.mean(), 109.0)
+
+if __name__ == "__main__":
+    t = unittest.TestLoader().loadTestsFromTestCase(TestStat)
+    unittest.TextTestRunner(verbosity=2).run(t)
