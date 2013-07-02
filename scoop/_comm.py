ef5edf67a1160a99f2a0c4ed44dfdf2e2bedd1ea
Yannick Hold <yannickhold@gmail.com>
2013-07-02 10:33:50 -0400

    * Replaced direct __hash__ calls to builtin hash() function for compatibility

--- scoop/_comm.py
+++ scoop/_comm.py
@@ -154,10 +154,10 @@ class ZMQCommunicator(object):
 
     def sendFuture(self, future):
         try:
-            if shared.getConst(future.callable.__hash__,
+            if shared.getConst(hash(future.callable),
                                timeout=0):
                 # Enforce name reference passing if already shared
-                future.callable = SharedElementEncapsulation(future.callable.__hash__)
+                future.callable = SharedElementEncapsulation(hash(future.callable))
             self.socket.send_multipart([b"TASK",
                                         pickle.dumps(future,
                                                      pickle.HIGHEST_PROTOCOL)])
@@ -166,7 +166,7 @@ class ZMQCommunicator(object):
             # TODO: use its fully qualified name
             scoop.logger.warn("Pickling Error: {0}".format(e))
             previousCallback = future.callable
-            future.callable = future.callable.__hash__
+            future.callable = hash(future.callable)
             self.socket.send_multipart([b"TASK",
                                         pickle.dumps(future,
                                                      pickle.HIGHEST_PROTOCOL)])
